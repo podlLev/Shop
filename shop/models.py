1aca6c608cd6bebd61848b625e4987c7026a2ef4
podlLev <pidlisnyilev@gmail.com>
2025-10-14 15:21:55 +0300

    Add Category and Product models with relations

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils.text import slugify
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    description = models.TextField(blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(0.0)]
+    )
+    product_qty = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
