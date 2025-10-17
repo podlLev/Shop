b5f251e914812283096ab4ab224faaefc19e5271
podlLev <pidlisnyilev@gmail.com>
2025-10-17 11:03:44 +0300

    Add routes in urls.py for DB table views

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='shop_home'),
+    path('categories/', views.CategoryListView.as_view(), name='category_list'),
+    path('categories/<slug:slug>/', views.ProductListByCategoryView.as_view(), name='product_list_by_category'),
+    path('products/', views.ProductListView.as_view(), name='product_list'),
 ]
