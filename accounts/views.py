db47bdce73507a9b2576565d04617b27d031172b
podlLev <pidlisnyilev@gmail.com>
2025-11-07 18:34:40 +0200

    Implement user custom login, registration, activation, and profile update views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,92 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth.views import LoginView
+from django.core.mail import send_mail
+from django.forms import ModelForm
+from django.shortcuts import redirect
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.views.generic import UpdateView, FormView
 
-# Create your views here.
+from accounts.forms import ProfileUpdateForm, UserUpdateForm, UserRegisterForm, CustomLoginForm
+from accounts.models import Profile
+from base import settings
+
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    authentication_form = CustomLoginForm
+    redirect_authenticated_user = True
+    next_page = reverse_lazy('shop_home')
+
+
+class RegisterView(FormView):
+    template_name = 'accounts/register.html'
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form : ModelForm):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.save()
+
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        token = default_token_generator.make_token(user)
+        activate_url = self.request.build_absolute_uri(
+            reverse_lazy('activate', kwargs={'uidb64': uid, 'token': token})
+        )
+
+        subject = 'Activate your account'
+        message = render_to_string('accounts/activation_email.html', {
+            'user': user,
+            'activate_url': activate_url,
+        })
+
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
+        messages.success(self.request, 'Account created! Please check your email to activate your account.')
+
+        return super().form_valid(form)
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Your account has been activated! You can now log in.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Activation link is invalid or has expired.')
+        return redirect('register')
+
+
+class ProfileView(LoginRequiredMixin, UpdateView):
+    template_name = 'accounts/profile.html'
+    model = Profile
+    form_class = ProfileUpdateForm
+    success_url = reverse_lazy('profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user.profile
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_form'] = UserUpdateForm(instance=self.request.user)
+        context['profile_form'] = context.pop('form', None)
+        return context
+
+    def form_valid(self, form):
+        user_form = UserUpdateForm(self.request.POST, instance=self.request.user)
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(self.request, 'Your profile has been updated!')
+        return super().form_valid(form)
--- base/settings.py
+++ base/settings.py
@@ -133,6 +133,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'sk8e-staff@gmail.com'
 
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'login'
