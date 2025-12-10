7078569fb5a84affffd1e4506097c64484ec9d43
podlLev <pidlisnyilev@gmail.com>
2025-12-10 14:37:11 +0200

    Merge remote-tracking branch 'origin/main'

--- base/settings.py
+++ base/settings.py
@@ -59,7 +59,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
